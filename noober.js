0194a15d13459e07085b10136c0a4c19fdc861e7
Maddie Guild <maddie.guild@kellogg.northwestern.edu>
2021-01-26 17:17:26 -0600

    tacos

--- noober.js
+++ noober.js
@@ -7,6 +7,8 @@ function pageLoad() {
   // to get started, try to write other parts of 'ride' to the JavaScript console in Chrome
   // e.g. ask 'ride' for its length or only grab the first member of the Array
 
+
+  
   let levelOfService = 'tacos'
 
   let passenger1Name
