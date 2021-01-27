bc5dc23142c8149cecb256b7e51b1b6ee8209942
Maddie Guild <maddie.guild@kellogg.northwestern.edu>
2021-01-27 11:59:18 -0600

    passenger 123

--- noober.js
+++ noober.js
@@ -7,23 +7,49 @@ function pageLoad() {
   // is refreshed or reloaded, the 'ride' variable will contain something different
   // to get started, try to write other parts of 'ride' to the JavaScript console in Chrome
   // e.g. ask 'ride' for its length or only grab the first member of the Array
- 
+
+console.log(ride)
+
+levelOfService = ['Noober X', 'Noober Purple', 'Noober XL']
+if (ride.length > 1 || ride[0]. numberOfPassengers ==1) {
+  levelOfService= 'Noober Pool'
+} else if (ride.purplerequested = true) {
+  levelOfService = 'Noober Pool'
+} else if(ride.numberOfPassengers > 3) {
+  levelOfService = 'Noober XL'
+} else(levelOfService = 'Noober X')
+
 let passenger1 = ride[0]
 let passenger2 = ride[1]
 let passenger3 = ride[2]
 
-levelOfService = ('Noober X', 'Noober Purple', 'Noober XL')
-
-IF (ride.length > 1) {
-  levelOfService = 'Noober Pool' 
-} else if (ride.purplerequested = true) {
-  levelOfService= 'Noober Purple'
-} else if (ride.numberOfPassengers > 3) {
-  levelOfService = 'Noober XL'
-} else {levelOfService= 'Noober X'}
-
 console.log(levelOfService)
 
+if (ride.length ==1) {
+  passenger1Name = passenger1.passengerDetils.first + ' ' + passenger1.passengerDetils.last
+  passenger1Phone = passenger1.passengerDetials.phoneNumber
+  Passenger1NumberOfPassengers = passenger1.passengerDetails.numberOfPassengers
+  passenger1PickupAddressLine1 =  passenger1.pickupLocation.address
+  passenger1PickupAddressLine2 = passenger1.pickupLocation.city + ', ' + passenger1.pickupLocation.state + ' ' + passenger1.pickupLocation.zip
+  passenger1DropoffAddressLine1 = passenger1.dropoffLocation.address
+  passenger1DropoffAddressLine2 = passenger1.dropoffLocation.city + ', ' + passenger1.dropoffLocation.state + ' ' + passenger1.dropoffLocation.zip
+} else if (ride.length == 2) {
+  passenger2Name = passenger2.passengerDetils.first + ' ' + passenger2.passengerDetils.last
+  passenger2Phone = passenger2.passengerDetials.phoneNumber
+  Passenger2NumberOfPassengers = passenger2.passengerDetails.numberOfPassengers
+  passenger2PickupAddressLine1 = passenger2.pickupLocation.address
+  passenger2PickupAddressLine2 = passenger2.pickupLocation.city + ', ' + passenger2.pickupLocation.state + ' ' + passenger2.pickupLocation.zip
+  passenger2DropoffAddressLine1 = passenger2.dropoffLocation.address
+  passenger2DropoffAddressLine2 = passenger2.dropoffLocation.city + ', ' + passenger2.dropoffLocation.state + ' ' + passenger2.dropoffLocation.zip
+} else if (ride.length == 3) {
+  passenger3Name = passenger3.passengerDetils.first + ' ' + passenger3.passengerDetils.last
+  passenger3Phone = passenger3.passengerDetials.phoneNumber
+  Passenger3NumberOfPassengers = passenger3.passengerDetails.numberOfPassengers
+  passenger3PickupAddressLine1 = passenger3.pickupLocation.address
+  passenger3PickupAddressLine2 = passenger3.pickupLocation.city + ', ' + passenger3.pickupLocation.state + ' ' + passenger3.pickupLocation.zip
+  passenger3DropoffAddressLine1 = passenger3.dropoffLocation.address
+  passenger3DropoffAddressLine2 = passenger3.dropoffLocation.city + ', ' + passenger3.dropoffLocation.state + ' ' + passenger3.dropoffLocation.zip
+}
   // these variables map to the elements on the finished page;
   // assign the proper value to each variable
 
